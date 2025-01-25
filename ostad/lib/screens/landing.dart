9d5df3bbd7073c733fbb51dc20c41f0fc9292f3b
Shahabuddin1122 <shavoddin54@gmail.com>
2025-01-25 22:05:12 +0600

    create an app and add themes

---
+++ ostad/lib/screens/landing.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Landing extends StatelessWidget {
+  const Landing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MCQ OSTAD"),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
