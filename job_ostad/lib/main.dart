c1a2eeaa2a4342d4180f98b425a2f06a3ec88030
Shahabuddin1122 <shavoddin54@gmail.com>
2025-03-20 21:33:25 +0600

    add question script

--- job_ostad/lib/main.dart
+++ job_ostad/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_ostad/screens/exam_script.dart';
 import 'package:job_ostad/screens/landing.dart';
 import 'package:job_ostad/utils/scheme.dart';
 
@@ -12,12 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: '/exam-script',
       theme: Scheme.lightTheme,
       darkTheme: Scheme.darkTheme,
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      routes: {'/': (context) => Landing()},
+      routes: {
+        '/': (context) => Landing(),
+        '/exam-script': (context) => ExamScript(),
+      },
     );
   }
 }
