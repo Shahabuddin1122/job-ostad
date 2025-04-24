1ff12b3637b673a21fda8653f04e61e67bd9b5d1
Shahabuddin1122 <shavoddin54@gmail.com>
2025-04-24 07:50:14 +0600

    add create_course api

--- backend/src/controllers/course.controller.js
+++ backend/src/controllers/course.controller.js
@@ -1,7 +1,33 @@
+const { imgbb } = require("../services/imagebb.service");
+const Course = require("../models/course.model");
+
 exports.course = (req, res) => {
   res.status(200).json({ success: true, message: "Course api works fine" });
 };
 
-exports.addCourse = (req, res) => {
-  res.json({ success: true });
+exports.addCourse = async (req, res) => {
+  try {
+    const {title, description, category, keywords} =  req.body
+    let course_image = req.files && req.files["course_image"] ? req.files["course_image"][0] : null;
+
+    if(!title || !description || !category || !keywords || !course_image){
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    course_image = await imgbb(course_image)
+    course_image = course_image.url
+    console.log(course_image)
+
+    const newCourse = await Course.create({
+      title,description,category,keywords,course_image
+    })
+
+    res.status(201).json({
+      message: "Course Create Successfully",
+      data: newCourse
+    })
+
+  }
+  catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
