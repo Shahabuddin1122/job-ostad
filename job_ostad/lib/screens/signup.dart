b20978326cea493c79b27afd975bd151eb4a6617
Shahabuddin1122 <shavoddin54@gmail.com>
2025-05-08 08:57:31 +0600

    add loading state at login and signup page

--- job_ostad/lib/screens/signup.dart
+++ job_ostad/lib/screens/signup.dart
@@ -5,6 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:job_ostad/utils/api_settings.dart';
 import 'package:job_ostad/utils/constants.dart';
 import 'package:job_ostad/utils/snackbar.dart';
+import 'package:job_ostad/widgets/loading.dart';
 import 'package:job_ostad/widgets/otpScreen.dart';
 
 class Signup extends StatefulWidget {
@@ -20,13 +21,17 @@ class _SignupState extends State<Signup> {
   final TextEditingController mobileController = TextEditingController();
   final TextEditingController passworcController = TextEditingController();
   String? selectEducation;
+  bool _isLoading = false;
 
   void sendOtp(BuildContext context) async {
+    setState(() {
+      _isLoading = true;
+    });
+
     ApiSettings apiSettings = ApiSettings(endPoint: 'auth/register');
     try {
       var data = {"phone_number": mobileController.text};
       final response = await apiSettings.postMethod(jsonEncode(data));
-      final a = jsonDecode(response.body);
       final body = {
         "username": userController.text,
         "email": emailController.text,
@@ -39,172 +44,190 @@ class _SignupState extends State<Signup> {
           context,
           MaterialPageRoute(builder: (context) => OtpScreen(data: body)),
         );
+      } else {
+        SnackbarUtils.showErrorSnackbar(context, "Failed to send OTP");
       }
     } catch (e) {
       SnackbarUtils.showErrorSnackbar(context, "Registration Failed");
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 40),
-              SvgPicture.asset(
-                'assets/images/svg/graduation hats.svg',
-                height: 200,
-                fit: BoxFit.none,
-              ),
-              const SizedBox(height: 24),
-              const Text(
-                "Create a new",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                "account",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 32),
-
-              TextField(
-                controller: userController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: 'User name',
-                  hintText: 'Enter your name',
-                  prefixIcon: const Icon(Icons.person),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  prefixIcon: const Icon(Icons.email),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: mobileController,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  labelText: 'Mobile Number',
-                  hintText: 'Enter your mobile number',
-                  prefixIcon: const Icon(Icons.phone),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1.0),
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                ),
-                child: DropdownButton<String>(
-                  hint: const Text(
-                    'Select Education',
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                  ),
-                  style: const TextStyle(color: Colors.black, fontSize: 16),
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(left: 12),
-                  isExpanded: true,
-                  value: selectEducation,
-                  items:
-                      ['SSC', 'HSC', 'Undergraduate', 'Graduate'].map((
-                        String item,
-                      ) {
-                        return DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(item),
-                        );
-                      }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectEducation = newValue!;
-                    });
-                  },
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: passworcController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                  prefixIcon: const Icon(Icons.lock),
-                  suffixIcon: IconButton(
-                    icon: const Icon(Icons.visibility_off),
-                    onPressed: () {}, // Add toggle logic later if needed
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    sendOtp(context);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text("Sign Up", style: TextStyle(fontSize: 18)),
-                ),
-              ),
-              const SizedBox(height: 24),
-              Row(
-                children: const [
-                  Expanded(child: Divider()),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
-                    child: Text("OR"),
-                  ),
-                  Expanded(child: Divider()),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Text("Already have an account?"),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/sign-in');
-                    },
-                    child: const Text(
-                      'Sign in',
-                      style: TextStyle(color: PRIMARY_COLOR),
+                  const SizedBox(height: 40),
+                  SvgPicture.asset(
+                    'assets/images/svg/graduation hats.svg',
+                    height: 200,
+                    fit: BoxFit.none,
+                  ),
+                  const SizedBox(height: 24),
+                  const Text(
+                    "Create a new",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    "account",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 32),
+
+                  TextField(
+                    controller: userController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      labelText: 'User name',
+                      hintText: 'Enter your name',
+                      prefixIcon: const Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                      prefixIcon: const Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: mobileController,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      labelText: 'Mobile Number',
+                      hintText: 'Enter your mobile number',
+                      prefixIcon: const Icon(Icons.phone),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1.0),
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: DropdownButton<String>(
+                      hint: const Text(
+                        'Select Education',
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                      style: const TextStyle(color: Colors.black, fontSize: 16),
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.only(left: 12),
+                      isExpanded: true,
+                      value: selectEducation,
+                      items:
+                          ['SSC', 'HSC', 'Undergraduate', 'Graduate'].map((
+                            String item,
+                          ) {
+                            return DropdownMenuItem<String>(
+                              value: item,
+                              child: Text(item),
+                            );
+                          }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectEducation = newValue!;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: passworcController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.visibility_off),
+                        onPressed: () {}, // Add toggle logic later if needed
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        sendOtp(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Row(
+                    children: const [
+                      Expanded(child: Divider()),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: Text("OR"),
+                      ),
+                      Expanded(child: Divider()),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account?"),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/sign-in');
+                        },
+                        child: const Text(
+                          'Sign in',
+                          style: TextStyle(color: PRIMARY_COLOR),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
+          if (_isLoading)
+            Container(
+              color: Colors.black.withAlpha(50),
+              child: const CustomLoadingIndicator(),
+            ),
+        ],
       ),
     );
   }
--- job_ostad/lib/widgets/otpScreen.dart
+++ job_ostad/lib/widgets/otpScreen.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:job_ostad/utils/api_settings.dart';
 import 'package:job_ostad/utils/constants.dart';
+import 'package:job_ostad/widgets/loading.dart';
 
 class OtpScreen extends StatefulWidget {
   final Map<String, dynamic> data;
@@ -13,6 +13,7 @@ class OtpScreen extends StatefulWidget {
 }
 
 class _OtpScreenState extends State<OtpScreen> {
+  bool isLoading = false;
   final List<TextEditingController> _controllers = List.generate(
     6,
     (_) => TextEditingController(),
@@ -39,17 +40,20 @@ class _OtpScreenState extends State<OtpScreen> {
       return;
     }
 
-    final Map<String, dynamic> payload = {
-      ...widget.data, // existing user data
-      "otp": otp, // add OTP
-    };
+    setState(() {
+      isLoading = true;
+    });
+
+    final Map<String, dynamic> payload = {...widget.data, "otp": otp};
 
     ApiSettings apiSettings = ApiSettings(endPoint: 'auth/verify-register');
 
     try {
       final response = await apiSettings.postMethod(jsonEncode(payload));
+      setState(() {
+        isLoading = false;
+      });
       if (response.statusCode == 201) {
-        print("Registration Verified Successfully");
         Navigator.popAndPushNamed(context, '/sign-in');
       } else {
         final responseBody = jsonDecode(response.body);
@@ -61,8 +65,9 @@ class _OtpScreenState extends State<OtpScreen> {
         Navigator.pop(context);
       }
     } catch (e) {
-      Navigator.pop(context);
-      print("Error during OTP verification: $e");
+      setState(() {
+        isLoading = false;
+      });
       ScaffoldMessenger.of(
         context,
       ).showSnackBar(SnackBar(content: Text("Something went wrong")));
@@ -71,74 +76,86 @@ class _OtpScreenState extends State<OtpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("OTP Verification")),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Verification code",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Text("We texted you a code", style: TextStyle(fontSize: 20)),
-              Text("Please enter it below", style: TextStyle(fontSize: 20)),
-              SizedBox(height: 40),
-              Row(
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(title: Text("OTP Verification")),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(6, (index) {
-                  return Container(
-                    width: 40,
-                    margin: EdgeInsets.symmetric(horizontal: 6),
-                    child: TextField(
-                      controller: _controllers[index],
-                      focusNode: _focusNodes[index],
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.center,
-                      maxLength: 1,
-                      style: TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
-                        counterText: '',
-                        border: OutlineInputBorder(),
-                      ),
-                      onChanged: (value) {
-                        if (value.isNotEmpty && index < 5) {
-                          _focusNodes[index + 1].requestFocus();
-                        } else if (value.isEmpty && index > 0) {
-                          _focusNodes[index - 1].requestFocus();
-                        }
-                      },
-                    ),
-                  );
-                }),
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Verification code",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  Text("We texted you a code", style: TextStyle(fontSize: 20)),
+                  Text("Please enter it below", style: TextStyle(fontSize: 20)),
+                  SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(6, (index) {
+                      return Container(
+                        width: 40,
+                        margin: EdgeInsets.symmetric(horizontal: 6),
+                        child: TextField(
+                          controller: _controllers[index],
+                          focusNode: _focusNodes[index],
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          maxLength: 1,
+                          style: TextStyle(fontSize: 20),
+                          decoration: InputDecoration(
+                            counterText: '',
+                            border: OutlineInputBorder(),
+                          ),
+                          onChanged: (value) {
+                            if (value.isNotEmpty && index < 5) {
+                              _focusNodes[index + 1].requestFocus();
+                            } else if (value.isEmpty && index > 0) {
+                              _focusNodes[index - 1].requestFocus();
+                            }
+                          },
+                        ),
+                      );
+                    }),
+                  ),
+                  SizedBox(height: 40),
+                  Text(
+                    "This helps us verify every user on our platform.",
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    "Didn't receive a code?",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
               ),
-              SizedBox(height: 40),
-              Text(
-                "This helps us verify every user on our platform.",
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
+            ),
+          ),
+          bottomNavigationBar: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: PRIMARY_COLOR),
+                onPressed: _submitOtp,
+                child: Text("Confirm", style: TextStyle(fontSize: 18)),
               ),
-              Text("Didn't receive a code?", style: TextStyle(fontSize: 16)),
-            ],
+            ),
           ),
         ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SizedBox(
-          width: double.infinity,
-          height: 50,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(backgroundColor: PRIMARY_COLOR),
-            onPressed: _submitOtp,
-            child: Text("Confirm", style: TextStyle(fontSize: 18)),
+        if (isLoading)
+          Container(
+            color: Colors.black.withAlpha(50),
+            child: const CustomLoadingIndicator(),
           ),
-        ),
-      ),
+      ],
     );
   }
 }
