883e9795c237536ddf235de280769e45c3fe5495
Shahabuddin1122 <shavoddin54@gmail.com>
2025-05-13 17:47:45 +0600

    add user statistics

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const {add_user_question_response, get_user_results, get_exam_script_by_result_id} = require("../controllers/user.controller");
+const {add_user_question_response, get_user_results, get_exam_script_by_result_id, get_user_info, get_user_stat} = require("../controllers/user.controller");
 const {authenticateToken} = require("../middlewares/auth.middleware");
 
 const router = express.Router()
 
 router.post('/add-user-question-response', authenticateToken, add_user_question_response)
 router.get('/get-user-result', authenticateToken, get_user_results)
-router.get('/get-exam-script', get_exam_script_by_result_id)
+router.get('/get-exam-script', authenticateToken, get_exam_script_by_result_id)
+router.get('/get-user-details', authenticateToken, get_user_info)
+router.get('/get-user-stat', authenticateToken, get_user_stat)
 
 module.exports = router;
