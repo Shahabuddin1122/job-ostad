3548a48408e8f8c329099c18ed89b3d3685ce91f
Shahabuddin1122 <shavoddin54@gmail.com>
2025-05-19 16:48:36 +0600

    add notification component

--- job_ostad/lib/screens/overview.dart
+++ job_ostad/lib/screens/overview.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:job_ostad/utils/custom_theme.dart';
+import 'package:job_ostad/widgets/notificationWidget.dart';
 
 class Overview extends StatelessWidget {
   const Overview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("Overview");
+    return SingleChildScrollView(
+      child: Padding(
+        padding: Theme.of(context).defaultPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 10.0,
+          children: [
+            Notificationwidget(
+              title: "Exam Reminder",
+              description: 'Your Math Final Exam starts tomorrow at 10:00 AM.',
+              date: "May 20, 2025",
+              time: "10: 30 AM",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
